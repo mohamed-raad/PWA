db9b45341641c1c189b931408de91ca1e9215c9f
mohamed-raad <mohamed.r.abbas99@gmail.com>
2024-09-10 14:13:40 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -3,8 +3,6 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
 
 const CACHE = "pwabuilder-page";
-
-// TODO: replace the following with the correct offline fallback page i.e.: const offlineFallbackPage = "offline.html";
 const offlineFallbackPage = "Index.html";
 
 self.addEventListener("message", (event) => {
@@ -15,8 +13,9 @@ self.addEventListener("message", (event) => {
 
 self.addEventListener('install', async (event) => {
   event.waitUntil(
-    caches.open(CACHE)
-      .then((cache) => cache.add(offlineFallbackPage))
+    caches.open(CACHE).then((cache) => {
+      return cache.addAll([offlineFallbackPage]);
+    })
   );
 });
 
@@ -37,10 +36,10 @@ self.addEventListener('fetch', (event) => {
         const networkResp = await fetch(event.request);
         return networkResp;
       } catch (error) {
-
         const cache = await caches.open(CACHE);
         const cachedResp = await cache.match(offlineFallbackPage);
-        return cachedResp;
+        return cachedResp || new Response('Offline page not available.', {status: 404});
       }
     })());
   }
+});
